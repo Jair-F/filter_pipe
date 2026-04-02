cd995264ff1fb9c80ee0850be7924c1918947741
Jair-F <jair.fehlauer@gmail.com>
2026-04-02 15:34:34 +0000

    testing on pipeline and not filters itself

--- tests/test_pipeline.py
+++ tests/test_pipeline.py
@@ -3,38 +3,86 @@ import pytest
 from src.pipeline.pipeline import Pipeline
 
 
-def test_pipeline_one_filter_moving_average():
-    input_values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    pipe = Pipeline('mavg(n=5)')
-
-    for value in input_values:
-        pipe.calc(value)
-
-    assert pipe.last_calc_value_float() == 8
+def test_pipeline_math_divide_no_spaces():
+    pipe = Pipeline('/2')
+    pipe.calc(10)
+    assert pipe.last_calc_value_float() == 5
 
 
-def test_pipeline_two_filters_moving_average_low_pass_single_input_value():
-    pipe = Pipeline(' mavg(n=10) | lpass(alpha=0.2) ')
+def test_pipeline_math_divide__minus_comma():
+    pipe = Pipeline('/-2.5')
+    pipe.calc(10)
+    assert pipe.last_calc_value_float() == -4
 
-    pipe.calc(2134)
 
-    assert pipe.last_calc_value_float() == 426.8
+def test_pipeline_math_divide_with_comma_no_spaces():
+    pipe = Pipeline('/2.2')
+    pipe.calc(22)
+    assert pipe.last_calc_value_float() == 10
+
+
+def test_pipeline_math_multiply_no_spaces():
+    pipe = Pipeline('*2')
+    pipe.calc(10)
+    assert pipe.last_calc_value_float() == 20
+
+
+def test_pipeline_math_multiply_minus_comma():
+    pipe = Pipeline('*-2.5')
+    pipe.calc(10)
+    assert pipe.last_calc_value_float() == -25
+
+
+def test_pipeline_math_multiply_with_comma_no_spaces():
+    pipe = Pipeline('*2.2')
+    pipe.calc(10)
+    assert pipe.last_calc_value_float() == 22
+
+
+def test_pipeline_math_add_with_comma_no_spaces():
+    pipe = Pipeline('+2.2')
+    pipe.calc(10)
+    assert pipe.last_calc_value_float() == 12.2
+
+
+def test_pipeline_math_add_no_comma_no_spaces():
+    pipe = Pipeline('+234')
+    pipe.calc(10)
+    assert pipe.last_calc_value_float() == 244
+
+
+def test_pipeline_math_subtract_with_comma_no_spaces():
+    pipe = Pipeline('-2.2')
+    pipe.calc(10)
+    assert pipe.last_calc_value_float() == 7.8
+
+
+def test_pipeline_math_subtract_no_comma_no_spaces():
+    pipe = Pipeline('-234')
+    pipe.calc(10)
+    assert pipe.last_calc_value_float() == -224
+
+
+def test_pipeline_math_divide_with_spaces_and_tabs():
+    pipe = Pipeline('   \t /  \t2   \t  ')
+    pipe.calc(10)
+    assert pipe.last_calc_value_float() == 5
 
 
 def test_pipeline_with_spaces_and_tabs():
-    pipe = Pipeline(' \t  mavg(n=10)   |  \t  lpass(alpha=0.2) \t ')
+    pipe = Pipeline(' \t  lpass(alpha=0.2)   |  \t  lpass(alpha=0.2) \t ')
 
-    pipe.calc(2134)
+    pipe.calc(100)
 
-    assert pipe.last_calc_value_float() == 426.8
+    assert pipe.last_calc_value_float() == 4
 
 
 def test_pipeline_without_any_space():
-    pipe = Pipeline('mavg(n=10)|lpass(alpha=0.2)')
+    pipe = Pipeline('hpass(alpha=0.8)|lpass(alpha=0.2)')
 
     pipe.calc(2134)
 
-    assert pipe.last_calc_value_float() == 426.8
+    assert pipe.last_calc_value_float() == 85.36
 
 
 def test_pipeline_two_filters_moving_average_low_pass_multiple_input_values():
